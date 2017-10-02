10183e9ec0a47e098a9c976358bcd6e900d78a3c
olddonny <reedtrekks@gmail.com>
2017-10-02 17:03:08 -0500

    hide/show buttons

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('#keyboard-upper-container').hide()
+    $(document).keydown(function(e){
+        if(e.keyCode==16){
+            $('#keyboard-lower-container').hide();
+            $('#keyboard-upper-container').show();
+            
+        };
+    });
+    $(document).keyup(function(e){
+        if(e.keyCode==16){
+            $('#keyboard-upper-container').hide();
+            $('#keyboard-lower-container').show();
+        };
+    });
+
+    $(document).keypress(function(e){
+            $('#'+e.keyCode).toggle.css('background-color','yellow');
+                
+   
+      
+    });
+    
+
+
+    
+});
+
+
